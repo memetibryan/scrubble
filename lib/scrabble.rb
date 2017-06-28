0d78efc488dba367b460a4f191e72497a56f7dcb
memetibryan <memetibryan@gmail.com>
2017-06-28 17:35:09 +0300

    By Bryan Memeti =============== Code cracked and bug fixed. Test passed successfully

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,7 +1,7 @@
 class String
+		array = []
+
 	define_method(:scrubble) do
-		total= 0
-		initial_char = 0
 
 		scrubbles = {"a" => 1, "b" => 2,"c" => 3,"d" => 4,"e" => 5,
 			"f" => 6,"g" => 7,"h" => 8,"i" => 9,"j" => 10,"k" => 11,
@@ -9,14 +9,18 @@ class String
 			"r" => 8,"s" => 19,"t" => 20,"u" => 21,"v" => 22,"w" => 23,
 			"x" => 24,"y" => 25,"z" => 26}
 
-		letter = self.split("")
-		char = letter[initial_char]
+		letter = self.split("").each{ |c|
+			array.push(scrubbles.fetch(c))
+		}
 
-		while initial_char <= letter.length do
-		   initial_char +=1	
-		   total = scrubbles.fetch(char)
-		   puts total
-		end
+		puts array.sum		
+		# char = letter[initial_char]
+
+		# while initial_char <= letter.length do
+		#    initial_char +=1	
+		#    total = scrubbles.fetch(char)
+		#    puts total
+		# end
 	end
 end
 
--- spec/scrabble_spec.rb
+++ spec/scrabble_spec.rb
@@ -1,9 +1,10 @@
 require('rspec')
 require('pry')
   require('scrabble')
+  require('descriptive_statistics')
 
   describe('scrubble') do
     it("returns a scrabble score for a letter") do
-    expect("amy".scrubble())==(1)
+    expect("yfvsdivs".scrubble())==(126.0)
     end
   end
